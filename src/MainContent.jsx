d4ca4f79dd07bd370a200f638dd2a9583abd0ea4
Matlawton <matlawton88@gmail.com>
2022-02-08 14:36:32 +0800

    Updates to maincontent

--- src/MainContent.jsx
+++ src/MainContent.jsx
@@ -2,6 +2,23 @@ import React, { Component } from "react";
 
 export default class MainContent extends Component {
   render() {
-    return <div>Hello from main content</div>;
+    return (
+      <div class="jumbotron">
+        <h1 class="display-4">Hello, world!</h1>
+        <p class="lead">
+          This is a simple hero unit, a simple jumbotron-style component for
+          calling extra attention to featured content or information.
+        </p>
+        <p>
+          It uses utility classes for typography and spacing to space content
+          out within the larger container.
+        </p>
+        <p class="lead">
+          <a class="btn btn-primary btn-lg" href="/#" role="button">
+            Learn more
+          </a>
+        </p>
+      </div>
+    );
   }
 }

32f4f3b275e8a5781a95074d098fa13da9409a0e
droucode <iamdrou@gmail.com>
2022-02-11 22:00:04 +1100

    Merch update form complete and correspoing styling in index.css incorporated and commented

--- src/MainContent.jsx
+++ src/MainContent.jsx
@@ -7,11 +7,39 @@ export default class MainContent extends Component {
   <p> A Social Enterprise that moves music deeper into our community</p>
     <a classButton="button" href="./Product" role="button">Wear It</a>
 </div>
+{/* Merch updates form */}
+    <div className="MerchUpdateForm">
+      <h1>Sign up to get updates on newly released Merch</h1>
+      <form>
+      <fieldset>
+         <label>
+           <p>First Name</p>
+           <input name="First Name" />
+         </label>
+         <label>
+           <p>Last Name</p>
+           <input name="Last Name" />
+         </label>
+         <label>
+           <p>Email</p>
+           <input name="Email" />
+         </label>
+
+       </fieldset>
+       <a classButton="button" href="./MerchLetter" role="button">Submit</a>
+      </form>
+    </div>
   </React.Fragment>
   )
       }
     }
     
+  
+
+
+
+
+
 
       
 

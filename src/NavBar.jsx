484d1318026c6d061df4bcd761072149b35a89a2
droucode <iamdrou@gmail.com>
2022-02-11 15:06:30 +1100

    finalised navbar, .jsx pages linked

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,68 +1,53 @@
 import React, { Component } from "react";
-import Login from "./Login";
+import { Link } from "react-router-dom";
 
 class NavBar extends Component {
   render() {
     return (
       <React.Fragment>
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark navbar-style">
-          <div className="container-fluid">
-            <a className="navbar-brand" href="/#">
-              MUSIC MAKES IT
-            </a>
-            {/* Below is the hamburger menu for mobile display */}
-            <button
-              className="navbar-toggler"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#navbarSupportedContent"
-              aria-controls="navbarSupportedContent"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-            >
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div
-              className="collapse navbar-collapse"
-              id="navbarSupportedContent"
-            >
-              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                <li className="nav-item">
-                  <a className="nav-link active" aria-current="page" href="/#">
-                    Home
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="/#">
-                    About
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="/#">
-                    Wear It
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="/#">
-                    Impact
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="./Login.jsx">
-                    Login
-                  </a>
-                </li>
-              </ul>
-              {/* <form className="d-flex">
-        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        <button className="btn btn-outline-success" type="submit">Search</button>
-    </form> */}
-            </div>
+          <a className="navbar-brand" href="/#">
+           MUSIC MAKES IT
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav mr-auto">
+              <li className="nav-item">
+                <Link to="/Login" className="nav-link">
+                  Login
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/Dashboard" className="nav-link">
+                  Dashboard
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/Product" className="nav-link">
+                  Wear It
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/ShoppingCart" className="nav-link">
+                  Shopping Cart
+                </Link>
+              </li>
+            </ul>
           </div>
         </nav>
       </React.Fragment>
     );
   }
 }
-
 export default NavBar;

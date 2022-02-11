a00129709dadeb90ba234e2df90b99ca51c08206
Matlawton <matlawton88@gmail.com>
2022-02-11 14:35:13 +0800

    Fixed navbar, changes switch to routes as per react docs for v6 react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import React, { Component } from "react";
 import NavBar from "./NavBar";
 import Dashboard from "./Dashboard";
 import Login from "./Login";
-import Product from "./Product"; 
+import Product from "./Product";
 import ShoppingCart from "./ShoppingCart";
-import { Route, Switch } from "react-router";
+import { Routes, Route } from "react-router";
 import { BrowserRouter } from "react-router-dom";
 
 export default class App extends Component {
@@ -13,12 +13,12 @@ export default class App extends Component {
       <BrowserRouter>
         <NavBar />
         <div className="container-fluid">
-          <Switch>
-            <Route path="/" exact component={Login} />
-            <Route path="/dashboard" exact component={Dashboard} />
-            <Route path="/customers" exact component={Product} />
-            <Route path="/cart" exact component={ShoppingCart} />
-          </Switch>
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/product" element={<Product />} />
+            <Route path="/shoppingcart" element={<ShoppingCart />} />
+          </Routes>
         </div>
       </BrowserRouter>
     );

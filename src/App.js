9766d44d1e6a9453dac76b9670aead220ca90935
Matlawton <matlawton88@gmail.com>
2022-02-12 14:51:58 +0800

    Added routes for signup page

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import Home from "./components/Home.js";
+import Dashboard from "./components/Dashboard.js";
+import Cart from "./components/Cart.js";
+import SignUp from "./SignUp.js";
+
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div className="App">
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/dashboard" element={<Dashboard />} />
+            <Route exact path="/cart" element={<Cart />} />
+            <Route exact path="/signup" element={<SignUp />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    );
+  }
+}
+
+export default App;
